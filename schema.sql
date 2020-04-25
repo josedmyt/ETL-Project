e446fd4a5fc477753f4c772ecdb4cf2eb62251c9
leahlindy <58431732+leahlindy@users.noreply.github.com>
2020-04-25 14:40:37 -0400

    remove file

--- schema.sql
+++ schema.sql
@@ -1,3 +1,17 @@
+-- Database: jobposting_db
+
+-- DROP DATABASE jobposting_db;
+
+CREATE DATABASE jobposting_db
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'C'
+    LC_CTYPE = 'C'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+DROP TABLE workopolis_postings;
+
 CREATE TABLE workopolis_postings (
   id INT PRIMARY KEY,
 	job_title TEXT,
@@ -5,3 +19,5 @@ CREATE TABLE workopolis_postings (
 	job_location TEXT,
 	job_salary TEXT,
 	job_summary TEXT);
+
+
